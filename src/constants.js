609fcd0fad34e2d0fb1bc4a67ec08ab15cf21054
Justin Thomas <justhomas@csumb.edu>
2021-10-10 20:43:33 -0600

    oauth frontend modifications

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = 'https://jt-cst438-be.herokuapp.com/';
+//export const SERVER_URL = 'https://jt-cst438-be.herokuapp.com/';
+export const SERVER_URL = 'http://localhost:3000/';
